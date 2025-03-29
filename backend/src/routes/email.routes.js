67a2ca65f19888926a9f200b0c399f060d4ec116
MiguelMartinez30 <168313312+Miguel-Antonio-Martinez-Jimenez@users.noreply.github.com>
2025-03-29 14:34:47 -0500

    Delete backend directory

--- backend/src/routes/email.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const EmailController = require('../controllers/email.controller');
-
-const router = express.Router();
-
-// Ruta unificada para validar cualquier tipo de email
-router.post('/validar-email', EmailController.validarEmail);
-
-module.exports = router;
